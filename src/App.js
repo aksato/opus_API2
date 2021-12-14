5e2728ee86b71a80a37fb233a095f303e2bce5be
Andre Sato <andre.kubagawa@usp.br>
2021-12-14 13:41:42 -0300

    Fix warning

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 
 function TaskTable(props) {
-  let { list, onDismiss, searchTerm} = props;
-  console.log(list)
-  if(!searchTerm){searchTerm = ""}
+  let { list, onDismiss, searchTerm } = props;
+  console.log(list);
+  if (!searchTerm) {
+    searchTerm = "";
+  }
   return (
-      <table>
-        <thead>
-          <tr>
-            <th>Subject</th>
-            <th>Start Date</th>
-            <th>Start Time</th>
-            <th>End Date</th>
-            <th>End Time</th>
-            <th>All Day Event</th>
-            <th>Description</th>
-            <th>Location</th>
-            <th>Private</th>
-          </tr>
-        </thead>
-        <tbody>
-          {list.filter(task => task.Subject.toLowerCase().includes(searchTerm.toLowerCase()))
+    <table>
+      <thead>
+        <tr>
+          <th>Subject</th>
+          <th>Start Date</th>
+          <th>Start Time</th>
+          <th>End Date</th>
+          <th>End Time</th>
+          <th>All Day Event</th>
+          <th>Description</th>
+          <th>Location</th>
+          <th>Private</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list
+          .filter((task) =>
+            task.Subject.toLowerCase().includes(searchTerm.toLowerCase())
+          )
           .map((task) => (
             <tr key={task.id}>
               <td>{task.Subject}</td>
@@ -38,28 +43,29 @@ function TaskTable(props) {
               </td>
             </tr>
           ))}
-        </tbody>
-      </table>
+      </tbody>
+    </table>
   );
 }
 
 function Search(props) {
-  const {searchTerm, handleInputChange, children} = props;
+  const { searchTerm, handleInputChange, children } = props;
   return (
-      <form class='white-box'>
-        {children} <input type="text" placeholder="Pesquise pelo nome da task"
-          name="searchTerm" value={searchTerm} 
-          onChange={(event) => handleInputChange(event)}/>
-      </form>
-    );
+    <form class="white-box">
+      {children}{" "}
+      <input
+        type="text"
+        placeholder="Pesquise pelo nome da task"
+        name="searchTerm"
+        value={searchTerm}
+        onChange={(event) => handleInputChange(event)}
+      />
+    </form>
+  );
 }
 
 function Button(props) {
-  const {
-    onClick,
-    className='',
-    children,
-  } = props;
+  const { onClick, className = "", children } = props;
 
   return (
     <button
@@ -94,91 +100,94 @@ function Button(props) {
 // }
 
 class App extends React.Component {
-  
   constructor(props) {
     super(props);
     this.state = { list: null };
   }
-  
+
   onCSV(objArray) {
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var array = typeof objArray != "object" ? JSON.parse(objArray) : objArray;
     console.log(array[0]);
-    var str = '';
+    var str = "";
 
     for (var i = -1; i < array.length; i++) {
-        var line = '';
-        if (i!=-1){
+      var line = "";
+      if (i !== -1) {
         for (var index in array[i]) {
-            if (index != 'id'){
-              if (line != '') line += ','
-              line += array[i][index];
-            }
-        }}
-        else {
-          line = 'Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private'
+          if (index !== "id") {
+            if (line !== "") line += ",";
+            line += array[i][index];
+          }
         }
+      } else {
+        line =
+          "Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private";
+      }
 
-        str += line + '\r\n';
+      str += line + "\r\n";
     }
-    
-    console.log(str)
 
-    var hiddenElement = document.createElement('a');
-    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(str);
-    hiddenElement.target = '_blank';
-    hiddenElement.download = 'output.csv';
+    console.log(str);
+
+    var hiddenElement = document.createElement("a");
+    hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(str);
+    hiddenElement.target = "_blank";
+    hiddenElement.download = "output.csv";
     hiddenElement.click();
     return str;
   }
 
   onDismiss(id) {
-    const updatedList = this.state.list.filter(item => item.id !== id);
+    const updatedList = this.state.list.filter((item) => item.id !== id);
     this.setState({ list: updatedList });
   }
 
   handleInputChange(event) {
     const target = event.target;
-    const value = 
-      target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
 
     this.setState({
-      [name]: value
+      [name]: value,
     });
   }
 
   componentDidMount() {
-    fetch('http://127.0.0.1:8000/api/v1/appsite/')
+    fetch("http://127.0.0.1:8000/api/v1/appsite/")
       .then((response) => response.json())
       .then((result) => this.setState({ list: result }))
       .catch((error) => error);
   }
 
   render() {
-    const {list, searchTerm} = this.state;
+    const { list, searchTerm } = this.state;
 
     if (!list) {
       return null;
     }
-    
+
     return (
       <div className="App">
-        <p><h2>Opus 2 Calendar</h2></p>
-        <p class = 'search'>
-        Pesquise uma task: 
-        <Search
-          searchTerm={searchTerm}
-          handleInputChange={(e) => this.handleInputChange(e)}
-        >
-        </Search></p>
-        { list ? (
-          <TaskTable 
-            list={list} 
+        <p>
+          <h2>Opus 2 Calendar</h2>
+        </p>
+        <p class="search">
+          Pesquise uma task:
+          <Search
+            searchTerm={searchTerm}
+            handleInputChange={(e) => this.handleInputChange(e)}
+          ></Search>
+        </p>
+        {list ? (
+          <TaskTable
+            list={list}
             searchTerm={searchTerm}
             onDismiss={(id) => this.onDismiss(id)}
           />
         ) : null}
-        <p><Button onClick={() => this.onCSV(list)}>Baixar CSV</Button></p>
+        <p>
+          <Button onClick={() => this.onCSV(list)}>Baixar CSV</Button>
+        </p>
       </div>
     );
   }
